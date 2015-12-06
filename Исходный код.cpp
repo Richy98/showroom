fdc80ed21c87b3b713614a65f0fcbaf012bfee9f
Richy98 <daniil-malanchak@rambler.ru>
2015-12-06 15:49:36 +0200

    Основа программы заложена. Создано 5 классов и метод для ожидания ввода пользователя любой кнопки для завершения программы.

---
+++ Исходный код.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+using namespace std;
+class Pictures
+{
+ 	
+};
+class Artists
+{
+
+};
+class Prices
+{
+
+};
+class Workers
+{
+
+};
+class People
+{
+
+};
+int main() {
+
+	cin.sync(); cin.get();
+	return 0;
+}
