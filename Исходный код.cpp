8008b5f88ec10e2f9e10dcf6925f2cd0ebf0b7d8
Richy98 <daniil-malanchak@rambler.ru>
2015-12-08 21:24:46 +0200

    Открытие файла..но хм, код кажись правильный, но не открывает. Сложно

--- Исходный код.cpp
+++ Исходный код.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include<fstream>
 #include<string>
+#include<cstdio>
+#include<stdio.h>
+#include <conio.h>
+#include <io.h>
+#define FNAME "D:/Scream297and396.jpg"
 using namespace std;
 class Pictures
 {
@@ -7,11 +13,13 @@ class Pictures
 };
 class Artists
 {
-
+private: string author;
+//public: 
 };
 class Prices
 {
-
+private: float cost;
+//public:  
 };
 class Workers
 {
@@ -22,7 +30,13 @@ class People
 
 };
 int main() {
-
-	cin.sync(); cin.get();
-	return 0;
+	char fname [50] =FNAME;
+	FILE *fout;
+	fout = fopen(FNAME,"r");
+	//FILE *fopen("Scream297and396.jpg", "r"); 
+	//cout << "Hello"; 
+	// ifstream infile;                         
+	// fstream file("1.txt");//, ios::in);
+	//cin.sync(); cin.get();
+	//return 0;
 }
